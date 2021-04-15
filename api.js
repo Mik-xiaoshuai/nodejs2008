ac0b338862d942f80b5cc8bc1610fa3a69b1e7e4
Mik-xiaoshuai <you@example.com>
2021-04-15 08:54:06 +0800

    api第二次提交

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const express = require('express')   //引入模块
+var mysql= require('mysql');   //引入模块
 const app = express()     //调用  express
 const port = 8080     //服务运算的端口
 
@@ -8,9 +9,24 @@ app.get('/User',function (req,res) {
 });
 
 app.get('/list',function(req,res){
-    res.send("欢迎访问用户接口")
-})
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'root',
+        database : 'vue2008'
+    });
 
+    connection.connect();
+
+    connection.query("select user_id,user_name,email from p_users limit 10",function (error, results, fields) {
+        // if (error) throw error;
+        // console.log('The solution is: ', results[0].solution);
+        // console.log(results)
+        res.send(JSON.stringify(results))
+    });
+
+    connection.end();
+})
 app.get('/', (req, res) => {   //路由写法
 
     const list=[
